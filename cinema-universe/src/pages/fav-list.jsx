b0766564c09e67e432594a4f8cb0da8ad980b7ab
Calvin Syamdhan <calvin.santoso1601@gmail.com>
2023-12-20 15:23:58 +0700

    update favoritList.jsx

--- cinema-universe/src/pages/fav-list.jsx
+++ cinema-universe/src/pages/fav-list.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CUHomeLogo from "../assets/CU-Home.png";
-import banner from "../assets/mandalorian 1.png";
-import mandalorianLogo from "../assets/MandoS3Logo 1.png";
-import TopGunM from "../assets/TopGun M-2.png";
-import TopGun from "../assets/TopGun M.png";
-import Avengers from "../assets/avengers-1.png";
-import Spidermen from "../assets/Spiderman.png";
-import Batman from "../assets/Batman v Superman.png";
-import Batman1 from "../assets/Batman v Superman-2.png";
-import Black_bird from "../assets/black bird.png";
-import BlackPanther from "../assets/BlackPanther.png";
+import CUHomeLogo from "../assets/img/favFilm/CU-Home.png";
+import banner from "../assets/img/favFilm/mandalorian 1.png";
+import mandalorianLogo from "../assets/img/favFilm/MandoS3Logo 1.png";
+import TopGunM from "../assets/img/favFilm/TopGun M-2.png";
+import TopGun from "../assets/img/favFilm/TopGun M.png";
+import Avengers from "../assets/img/favFilm/avengers-1.png";
+import Spidermen from "../assets/img/favFilm/Spiderman.png";
+import Batman from "../assets/img/favFilm/Batman v Superman.png";
+import Batman1 from "../assets/img/favFilm/Batman v Superman-2.png";
+import Black_bird from "../assets/img/favFilm/black bird.png";
+import BlackPanther from "../assets/img/favFilm/BlackPanther.png";
 
-import { FaStar, FaPlay, FaPlus, FaBookmark, FaBookmarkFill } from "react-icons/fa";
+import {
+  FaStar,
+  FaPlay,
+  FaPlus,
+  FaBookmark,
+  FaBookmarkFill,
+} from "react-icons/fa";
 
 function Favorite() {
   const favoriteMovies = [
@@ -42,42 +48,72 @@ function Favorite() {
           <div>
             <ul className="flex flex-row space-x-4">
               <li>
-                <Link to="/" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Home</span>
+                <Link
+                  to="/"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Home
+                  </span>
                 </Link>
               </li>
-              <li>
-                <a href="#" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Movies</span>
+              {/* <li>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Movies
+                  </span>
                 </a>
-              </li>
+              </li> */}
               <li>
                 <img src={CUHomeLogo} alt="logo" />
               </li>
-              <li>
-                <a href="#" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Series</span>
+              {/* <li>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Series
+                  </span>
                 </a>
-              </li>
+              </li> */}
               <li>
-                <Link to="/fav-list" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Favorite</span>
+                <Link
+                  to="/fav-list"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Favorite
+                  </span>
                 </Link>
               </li>
             </ul>
           </div>
           <div>
             <ul className="flex flex-row gap-2">
-              <li>
-                <a href="#" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Login</span>
+              {/* <li>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Login
+                  </span>
                 </a>
               </li>
               <li>
-                <a href="#" className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah">
-                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">Register</span>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center justify-center p-0.5  text-center text-sm text-white rounded-full group bg-gradient-to-r from-merah-hati to-merah-cerah"
+                >
+                  <span className="rounded-full relative w-24  transition-all py-1 bg-white dark:bg-merah-hati group-hover:bg-opacity-0">
+                    Register
+                  </span>
                 </a>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
@@ -85,66 +121,77 @@ function Favorite() {
 
       {/* banner */}
       <section className="bg-merah-hati relative w-full h-screen justify-center items-center">
-  <img src={banner} className="absolute object-cover w-full h-full" alt="Banner" />
+        <img
+          src={banner}
+          className="absolute object-cover w-full h-full"
+          alt="Banner"
+        />
 
-  <div className="relative z-1 w-1/2 py-12 px-12">
-    <img src={mandalorianLogo} className="py-8 justify-center" />
+        <div className="relative z-1 w-1/2 py-12 px-12">
+          <img src={mandalorianLogo} className="py-8 justify-center" />
 
-    {/* rating */}
-    <div className="flex items-end pt-3">
-      <div className="flex items-start">
-        <FaStar size={24} color="#ffc107" />
-        <FaStar size={24} color="#ffc107" />
-        <FaStar size={24} color="#ffc107" />
-        <FaStar size={24} color="#ffc107" />
-        <FaStar size={24} color="#e4e5e9" />
-      </div>
-      <span className="ml-2 text-white">4.0</span>
-    </div>
+          {/* rating */}
+          <div className="flex items-end pt-3">
+            <div className="flex items-start">
+              <FaStar size={24} color="#ffc107" />
+              <FaStar size={24} color="#ffc107" />
+              <FaStar size={24} color="#ffc107" />
+              <FaStar size={24} color="#ffc107" />
+              <FaStar size={24} color="#e4e5e9" />
+            </div>
+            <span className="ml-2 text-white">4.0</span>
+          </div>
 
-    <h4 className="text-white font-sm font-200 flex-row pt-4 gap-4">
-      <span className="pr-4"> Action </span> |
-      <span className="px-4"> Adventure </span> |
-      <span className="px-4"> Fantasy </span>
-    </h4>
+          <h4 className="text-white font-sm font-200 flex-row pt-4 gap-4">
+            <span className="pr-4"> Action </span> |
+            <span className="px-4"> Adventure </span> |
+            <span className="px-4"> Fantasy </span>
+          </h4>
 
-    {/* buttonGroup */}
-    <div className="flex pt-4 gap-4">
-      <button className="bg-merah-cerah hover:bg-zinc-700 text-white font-bold p-4 rounded-full focus:outline-none focus:shadow-outline">
-        <FaPlay size={24} />
-      </button>
+          {/* buttonGroup */}
+          <div className="flex pt-4 gap-4">
+            <button className="bg-merah-cerah hover:bg-zinc-700 text-white font-bold p-4 rounded-full focus:outline-none focus:shadow-outline">
+              <FaPlay size={24} />
+            </button>
 
-      <button className="bg-merah-cerah hover:bg-zinc-700 text-white font-bold p-4 rounded-full focus:outline-none focus:shadow-outline">
-        <FaPlus size={24} />
-      </button>
-    </div>
+            <button className="bg-merah-cerah hover:bg-zinc-700 text-white font-bold p-4 rounded-full focus:outline-none focus:shadow-outline">
+              <FaPlus size={24} />
+            </button>
+          </div>
 
-    <div className="pt-4">
-      <p className="text-white text-justify">
-        The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.
-      </p>
-    </div>
-  </div>
-</section>
+          <div className="pt-4">
+            <p className="text-white text-justify">
+              The travels of a lone bounty hunter in the outer reaches of the
+              galaxy, far from the authority of the New Republic.
+            </p>
+          </div>
+        </div>
+      </section>
 
-{/* Movie list */}
-<section className="bg-merah-hati mt-1 py-4">
+      {/* Movie list */}
+      <section className="bg-merah-hati py-4">
         <div className="max-w-screen-xl mx-auto p-10">
-          <div className="text-white font-bold text-xl mb-4">My List</div>
+          <div className="text-white font-bold text-xl mb-10">My List</div>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-8">
             {favoriteMovies.map((movie, index) => (
               <div
                 key={index}
                 className="w-full h-48 transform transition-transform hover:scale-110 relative"
               >
-                <img src={movie.image} className="w-full h-full object-cover rounded-lg" alt={movie.title} />
+                <img
+                  src={movie.image}
+                  className="w-full h-full object-cover rounded-lg"
+                  alt={movie.title}
+                />
                 <div className="absolute bottom-0 left-0 w-full h-full px-4 pb-4 flex flex-col items-start">
                   <div className="flex items-start">
                     {/* Display single star rating */}
                     <FaStar size={15} color="#ffc107" />
                     <span className="ml-1 text-white">{movie.rating}</span>
                   </div>
-                  <h4 className="text-white font-semi text-lg">{movie.title}</h4>
+                  <h4 className="text-white font-semi text-lg">
+                    {movie.title}
+                  </h4>
                   <button className="mt-auto bg-transparan hover:bg-zinc-700 text-white font-bold p-2 rounded-full focus:outline-none focus:shadow-outline">
                     <FaBookmark size={15} />
                   </button>
