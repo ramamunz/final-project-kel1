551e310fd34aa58e22b7fd815674588480820f6a
AdityWildan <aditya.wildn@gmail.com>
2023-12-24 22:41:51 +0700

    Error di tambah favorit

--- cinema-universe/src/pages/favoritFilm.jsx
+++ cinema-universe/src/pages/favoritFilm.jsx
@@ -21,22 +21,32 @@ import {
 } from "react-icons/fa";
 
 function Favorite( {favoritFilm, setFavoritFilm} ) {
-  const favoriteMovies = [
-    { title: "Top Gun: Maverick", image: TopGunM, rating: 4 },
-    { title: "Top Gun: Maverick", image: TopGun, rating: 4 },
-    { title: "Avengers Infinity War", image: Avengers, rating: 4.5 },
-    { title: "Spider-Man: No Way Home", image: Spidermen, rating: 5 },
-    { title: "Batman v Superman", image: Batman, rating: 3.5 },
-    { title: "Black Bird", image: Black_bird, rating: 4 },
-    { title: "Black Panther", image: BlackPanther, rating: 4.5 },
-    { title: "Top Gun: Maverick", image: TopGunM, rating: 4 },
-    { title: "Top Gun: Maverick", image: TopGun, rating: 4 },
-    { title: "Avengers Infinity War", image: Avengers, rating: 4.5 },
-    { title: "Spider-Man: No Way Home", image: Spidermen, rating: 5 },
-    { title: "Batman v Superman", image: Batman1, rating: 3.5 },
-    { title: "Black Bird", image: Black_bird, rating: 4 },
-    { title: "Black Panther", image: BlackPanther, rating: 4.5 },
-  ];
+  // const favoriteMovies = [
+  //   { title: "Top Gun: Maverick", image: TopGunM, rating: 4 },
+  //   { title: "Top Gun: Maverick", image: TopGun, rating: 4 },
+  //   { title: "Avengers Infinity War", image: Avengers, rating: 4.5 },
+  //   { title: "Spider-Man: No Way Home", image: Spidermen, rating: 5 },
+  //   { title: "Batman v Superman", image: Batman, rating: 3.5 },
+  //   { title: "Black Bird", image: Black_bird, rating: 4 },
+  //   { title: "Black Panther", image: BlackPanther, rating: 4.5 },
+  //   { title: "Top Gun: Maverick", image: TopGunM, rating: 4 },
+  //   { title: "Top Gun: Maverick", image: TopGun, rating: 4 },
+  //   { title: "Avengers Infinity War", image: Avengers, rating: 4.5 },
+  //   { title: "Spider-Man: No Way Home", image: Spidermen, rating: 5 },
+  //   { title: "Batman v Superman", image: Batman1, rating: 3.5 },
+  //   { title: "Black Bird", image: Black_bird, rating: 4 },
+  //   { title: "Black Panther", image: BlackPanther, rating: 4.5 },
+  // ];
+
+  // function handleDeleteFavorite (id){
+  //   const newFavorites = favoritFilm.filter(item => item.id !== payload.id);
+  //   setFavoritFilm(newFavorites);
+  // }
+
+  function handleDeleteFavorite(id) {
+    const newFavorites = favoritFilm.filter((item) => item.id !== id);
+    setFavoritFilm(newFavorites);
+  }
 
   return (
     <>
@@ -173,31 +183,60 @@ function Favorite( {favoritFilm, setFavoritFilm} ) {
         <div className="max-w-screen-xl mx-auto p-10">
           <div className="text-white font-bold text-xl mb-4">My List</div>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-8">
-            {favoritFilm.map(movie => (
-              <div
-                key={movie.id}
-                className="w-full h-48 transform transition-transform hover:scale-110 relative"
-              >
-                <img
-                  src={movie.poster}
-                  className="w-full h-full object-cover rounded-lg"
-                  alt={movie.title}
-                />
-                <div className="absolute bottom-0 left-0 w-full h-full px-4 pb-4 flex flex-col items-start">
-                  <div className="flex items-start">
-                    {/* Display single star rating */}
-                    <FaStar size={15} color="#ffc107" />
-                    <span className="ml-1 text-white">{movie.rating}</span>
+            <div>
+              {favoritFilm.map(movie => (
+                <div
+                  key={movie.id}
+                  className="w-full h-48 transform transition-transform hover:scale-110 relative"
+                >
+                  <img
+                    src={movie.poster}
+                    className="w-full h-full object-cover rounded-lg"
+                    alt={movie.title}
+                  />
+                  <div className="absolute bottom-0 left-0 w-full h-full px-4 pb-4 flex flex-col items-start">
+                    <div className="flex items-start">
+                      {/* Display single star rating */}
+                      <FaStar size={15} color="#ffc107" />
+                      <span className="ml-1 text-white">{movie.rating}</span>
+                    </div>
+                    <h4 className="text-white font-bold text-lg">
+                      {movie.title}
+                    </h4>
+                    <button onClick={() => handleDeleteFavorite(movie.id)} className="mt-auto bg-transparan hover:bg-zinc-700 text-white font-bold p-2 rounded-full focus:outline-none focus:shadow-outline">
+                      <FaBookmark size={15} />
+                    </button>
                   </div>
-                  <h4 className="text-white font-bold text-lg">
-                    {movie.title}
-                  </h4>
-                  <button className="mt-auto bg-transparan hover:bg-zinc-700 text-white font-bold p-2 rounded-full focus:outline-none focus:shadow-outline">
-                    <FaBookmark size={15} />
-                  </button>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div>
+              {favoritFilm.map(movie => (
+                <div
+                  key={movie.id}
+                  className="w-full h-48 transform transition-transform hover:scale-110 relative"
+                >
+                  <img
+                    src={movie.poster}
+                    className="w-full h-full object-cover rounded-lg"
+                    alt={movie.title}
+                  />
+                  <div className="absolute bottom-0 left-0 w-full h-full px-4 pb-4 flex flex-col items-start">
+                    <div className="flex items-start">
+                      {/* Display single star rating */}
+                      <FaStar size={15} color="#ffc107" />
+                      <span className="ml-1 text-white">{movie.rating}</span>
+                    </div>
+                    <h4 className="text-white font-bold text-lg">
+                      {movie.title}
+                    </h4>
+                    <button onClick={() => handleDeleteFavorite(movie.id)} className="mt-auto bg-transparan hover:bg-zinc-700 text-white font-bold p-2 rounded-full focus:outline-none focus:shadow-outline">
+                      <FaBookmark size={15} />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
